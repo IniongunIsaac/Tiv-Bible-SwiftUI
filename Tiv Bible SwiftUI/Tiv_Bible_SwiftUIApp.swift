9e725779ce505a2119057d61d9861f247ad39048
Isaac Iniongun <iniongunisaac@gmail.com>
2021-06-18 11:07:27 +0100

    Initial Commit

---
+++ Tiv Bible SwiftUI/Tiv_Bible_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tiv_Bible_SwiftUIApp.swift
+//  Tiv Bible SwiftUI
+//
+//  Created by Isaac Iniongun on 18/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct Tiv_Bible_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
