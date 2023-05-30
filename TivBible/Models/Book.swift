186383161f0d4d19b809afba1a9e21883c7f2a0d
Isaac Iniongun <iniongunisaac@gmail.com>
2023-05-30 18:02:25 +0100

    Initial project creation

---
+++ TivBible/Models/Book.swift
@@ -0,0 +1,12 @@
+//
+//  Book.swift
+//  TivBible
+//
+//  Created by Isaac Iniongun on 30/05/2023.
+//
+
+import Foundation
+
+struct Book {
+    
+}
---
+++ TivBible/Models/Chapter.swift
@@ -0,0 +1,12 @@
+//
+//  Chapter.swift
+//  TivBible
+//
+//  Created by Isaac Iniongun on 30/05/2023.
+//
+
+import Foundation
+
+struct Chapter {
+    
+}
